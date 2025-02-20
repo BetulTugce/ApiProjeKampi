eaaef582686ad3cecfb04d38aef64768269c7cb3
Betül Tuğçe Dikdoğmuş <betultugcedikdogmus@gmail.com>
2025-02-20 21:00:35 +0300

    chef icin api controller yazildi

---
+++ ApiProjeKampi.WebApi/Controllers/ChefsController.cs
@@ -0,0 +1,62 @@
+﻿using ApiProjeKampi.WebApi.Contexts;
+using ApiProjeKampi.WebApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChefsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public ChefsController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetChefs()
+        {
+            var values = _context.Chefs.ToList();
+            return Ok(values);
+        }
+
+        [HttpGet("GetChef")]
+        public IActionResult GetChef(int id)
+        {
+            return Ok(_context.Chefs.Find(id));
+        }
+
+        [HttpPost]
+        public IActionResult CreateChef(Chef chef)
+        {
+            _context.Chefs.Add(chef);
+            _context.SaveChanges();
+            return StatusCode(201);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateChef(Chef chef)
+        {
+            _context.Chefs.Update(chef);
+            _context.SaveChanges();
+            return Ok("Şef başarıyla güncellendi!");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteChef(int id)
+        {
+            var chef = _context.Chefs.Find(id);
+            if (chef is not null)
+            {
+                _context.Chefs.Remove(chef);
+                _context.SaveChanges();
+
+                return Ok("İşlem başarılı!");
+            }
+
+            return NotFound();
+        }
+    }
+}
