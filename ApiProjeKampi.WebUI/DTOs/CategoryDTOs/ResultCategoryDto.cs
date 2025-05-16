bbb7b7dc5da986ecc1bfbffcd297c95c4be72b39
Betül Tuğçe Dikdoğmuş <betultugcedikdogmus@gmail.com>
2025-05-16 22:12:57 +0300

    25.ders tamamlandi.

---
+++ ApiProjeKampi.WebUI/DTOs/CategoryDTOs/ResultCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiProjeKampi.WebUI.DTOs.CategoryDTOs
+{
+    public class ResultCategoryDto
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
