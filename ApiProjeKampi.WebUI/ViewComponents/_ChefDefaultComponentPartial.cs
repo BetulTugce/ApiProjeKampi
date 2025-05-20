352a08c31bd1c04afbec75d7c07534e8e8ad45a3
Betül Tuğçe Dikdoğmuş <betultugcedikdogmus@gmail.com>
2025-05-20 21:09:09 +0300

    30.ders tamamlandi.

---
+++ ApiProjeKampi.WebUI/ViewComponents/_ChefDefaultComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using ApiProjeKampi.WebUI.DTOs.ChefDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi.WebUI.ViewComponents
+{
+    public class _ChefDefaultComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ChefDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient("default");
+            var responseMessage = await client.GetAsync("https://localhost:7017/api/Chefs");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultChefDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
